617d46b6a8a7d265bf4df58dacc187a112e74859
csyms-ecol <78804167+csyms-ecol@users.noreply.github.com>
2021-04-09 11:25:46 +0100

    Add files via upload

---
+++ docs/_build/jupyter_execute/landing-page.py
@@ -0,0 +1,33 @@
+# SafetyNet Technologies data analytics manifesto
+Maintainer: Craig Syms  
+<craig@sntech.co.uk>  
+
+## About this document
+This page serves as record, a manual, and a repository of information on the data analytics projects within SafetyNet Technologies.  
+
+Data analytics and analysis is a very interactive process. In the course of developing a line of investigation. we go in and out of a lot of rabbit holes. Some lead us somewhere useful, many are dead ends. However, even the dead ends guide analysis in some way. Documenting what is tried, evaluated and either retained or discarded is important to both anticipate and safeguard against challenges, and also to provide repeatability of results.
+
+This page is written using Jupyter notebook and the book is compiled by converting the markdown text to html and, where required, submitting and executing any code. So the book can be updated with new data as they come to hand. This is my first foray into it, and when I get more used to it I will expand the capabilities of the book.  
+
+Who is this intended for? Anyone in SNTech who is interested in the data analytics process. Much of the material in the book is either contained in the programming environment I use day-to-day (Jupyter Notebook), or added as explanatory material in report forms. So this is just an alternative or extension of the same material.
+
+Any comments or queries, please contact me <craig@sntech.co.uk>.
+
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+:caption: MIMIR database
+
+mimir/mimirstructure
+```
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+:caption: Trial design and statistical power
+
+power/trialpower
+```
